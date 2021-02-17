6b0df8b73bbd80810bfb5426bc922cfec34c44f2
Matheusdk75 <matheus.cbmdk@gmail.com>
2021-02-16 21:30:13 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Sistema de cadastro e listagem de lives que já foram realizadas ou que ainda irão acontecer.
+
+Tecnologias utilizadas:
+ 1. API:
+	- MongoDB
+	- Spring Boot
+2. Front-End:
+	- Angular (com Material UI)
---
+++ agendalive/src/app/shared/service/live.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Live } from '../model/live.model';
+import { responsePageable } from '../model/responsePageable.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LiveService {
+
+  apiUrl = 'http://localhost:8080/lives';
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':'application/json'
+    })
+  }
+
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  public getLivesWithFlag(flag: string): Observable<responsePageable>{
+    return this.httpClient.get<responsePageable>(this.apiUrl+'?flag='+flag);
+  }
+
+  public postLives(live: any): Observable<Live>{
+    return this.httpClient.post<any>(this.apiUrl, live, this.httpOptions);
+  }
+}
